70730a3cdac97ac6696c6bd2a2239315a67eeedd
Artur10021 <arturzelkovskiy@gmail.com>
2023-12-26 18:19:02 +0300

    added .prettierrc file, added node_modules to gitignore

--- client/src/trpc.ts
+++ client/src/trpc.ts
@@ -1,11 +1,10 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
-// здесь могут возникнуть проблемы при использовании синонимов путей (type aliases)
 import { AppRouter } from "../../server/src/index";
 
 export const trpc = createTRPCProxyClient<AppRouter>({
-  links: [
-    httpBatchLink({
-      url: "http://localhost:4000/trpc",
-    }),
-  ],
+    links: [
+        httpBatchLink({
+            url: "http://localhost:4000/trpc",
+        }),
+    ],
 });
