91d30b8340851cc7b2042b39579a2eb43c1909a0
Artur10021 <arturzelkovskiy@gmail.com>
2023-12-26 17:40:13 +0300

    deleted dist dir, added dist dir to .gitignore

--- server/dist/context.js
+++
@@ -1,4 +0,0 @@
-const createContext = ({ req, res, }) => {
-    return {};
-};
-export default createContext;
--- server/dist/router.js
+++
@@ -1,8 +0,0 @@
-import { router } from "./trpc.js";
-import userRouter from "./employees/router.js";
-import departmentRouter from "./departments/router.js";
-const appRouter = router({
-    employee: userRouter,
-    department: departmentRouter,
-});
-export default appRouter;
