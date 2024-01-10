411e8456bdd13907075ef1f20c0cd8bcb4c67d47
Artur10021 <arturzelkovskiy@gmail.com>
2024-01-10 11:49:43 +0300

    fixed styles for Header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,22 +1,20 @@
 import React from "react";
-import styles from "./styles/Header.module.scss";
+import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
     return (
-        <div className={styles.headWrapper}>
-            <header className={styles.header}>
-                <div className={styles.namePageBlock}>
-                    <Link to={"/"}>Dashboard</Link>
-                </div>
-                <div className={styles.namePageBlock}>
-                    <Link to={"/departments"}>Departments</Link>
-                </div>
-                <div className={styles.namePageBlock}>
-                    <Link to={"/employees"}>Employees</Link>
-                </div>
-            </header>
-        </div>
+        <header className={styles.header}>
+            <div>
+                <Link to={"/"}>Dashboard</Link>
+            </div>
+            <div>
+                <Link to={"/departments"}>Departments</Link>
+            </div>
+            <div>
+                <Link to={"/employees"}>Employees</Link>
+            </div>
+        </header>
     );
 };
 
